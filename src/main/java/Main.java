d3f4828a075488d820ca8f32b0fa6f1335c9f2b0
charlesygl <charlesygl@gmail.com>
2016-03-23 22:03:08 -0400

    try6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,10 +28,11 @@ public class Main {
     get("/hello", (req, res) -> {
           RelativisticModel.select();
 
-          String energy = System.getenv().get("ENERGY");
-
-          Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
-          return "E=mc^2: " + energy + " = " + m.toString();
+        //   String energy = System.getenv().get("ENERGY");
+          //
+        //   Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
+        //   return "E=mc^2: " + energy + " = " + m.toString();
+        return "Hello !!!!!!!"
         });
 
     /*get("/", (request, response) -> {

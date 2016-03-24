ff99a83b6d276dfaa27ef2dfa5c84ded50b5b862
charlesygl <charlesygl@gmail.com>
2016-03-24 01:45:13 -0400

    try13

--- src/main/java/UserService.java
+++ src/main/java/UserService.java
@@ -1,13 +1,20 @@
 import java.util.*;
+
 public class UserService {
+	private List<User> users = new ArrayList<>();
+	public UserService(){
+		users.add(new User("1","g","g@gmail.com"));
+		users.add(new User("2","m","m@gmail.com"));
+		
+	}
+	
+	
+	public List<User> getAllUsers(){
+		return users;
+		
+	}
 
-    private List<User> allUsers = new ArrayList<>();
 
-    public UserService(){
-        allUsers.add(new User("1", "G", "g@gmail.com"));
-        allUsers.add(new User("2", "M", "m@gmail.com"));
-    }
-    public List<User> getAllUsers(){
-        return allUsers;
-    }
+
+
 }

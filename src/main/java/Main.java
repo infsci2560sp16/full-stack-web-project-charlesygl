6429c0e1067c4ae39b12afaae81c4dcf5ed186ad
charlesygl <charlesygl@gmail.com>
2016-03-23 21:41:39 -0400

    try3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,6 +22,7 @@ public class Main {
 
   public static void main(String[] args) {
 
+     port(Integer.valueOf(System.getenv("PORT")));
      staticFileLocation("/public");
 
      get("/db", (req, res) -> {

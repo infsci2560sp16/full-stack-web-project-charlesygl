43f36bed846b2a72d2331ab2251c2dcd3ba9387f
charlesygl <charlesygl@gmail.com>
2016-03-23 21:31:53 -0400

    try

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,27 +22,7 @@ public class Main {
 
   public static void main(String[] args) {
 
-      FreeMarkerEngine freeMarkerEngine = new FreeMarkerEngine();
-      Configuration freeMarkerConfiguration = new Configuration();
-      freeMarkerConfiguration.setTemplateLoader(new ClassTemplateLoader(BlogService.class, "/"));
-      freeMarkerEngine.setConfiguration(freeMarkerConfiguration);
-
-
-
-      // get all post (using HTTP get method)
-      get("/posts", (request, response) -> {
-          if (shouldReturnHtml(request)) {
-              response.status(200);
-              response.type("text/html");
-              Map<String, Object> attributes = new HashMap<>();
-              attributes.put("posts", model.getAllPosts());
-              return freeMarkerEngine.render(new ModelAndView(attributes, "posts.ftl"));
-          } else {
-              response.status(200);
-              response.type("application/json");
-              return dataToJson(model.getAllPosts());
-          }
-      });
+      get("/hello", (req, res) -> "Hello World")
 
   }
 

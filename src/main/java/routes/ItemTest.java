d85d32a1a53d230cbf963d84f7e093eb8829bc41
charlesygl <charlesygl@gmail.com>
2016-03-30 03:22:12 -0400

    try

--- src/main/java/routes/ItemTest.java
+++ src/main/java/routes/ItemTest.java
@@ -15,6 +15,17 @@ import com.heroku.sdk.jdbc.DatabaseUrl;
 import itemservices.*;
 import itemdetails.*;
 
+import java.io.StringWriter;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 public class ItemTest {
 
   public ItemTest() {
@@ -134,7 +145,8 @@ get("/dbinsert", (req, res) -> {
 
 get("/xmlpage", (req, res) -> {
     	Connection connection = null;
-    		
+    	res.type("application/xml");
+
     	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 	    DocumentBuilder builder = factory.newDocumentBuilder();
 	    Document doc = builder.newDocument();

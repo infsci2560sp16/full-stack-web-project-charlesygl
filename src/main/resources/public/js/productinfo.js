b4b3d93dc17b66f8337450cd8b91bd4224c61c36
charlesygl <charlesygl@gmail.com>
2016-04-07 19:07:25 -0400

    try

--- src/main/resources/public/js/productinfo.js
+++ src/main/resources/public/js/productinfo.js
@@ -11,17 +11,17 @@ function getDataXML() {
 function handleDataXML(data) {
     alert("Hello2");
     $(data).find('itemDetails').each(function(){
-    var itemID = $(this).find('itemID').text();
-    var itemName = $(this).find('itemName').text();
-    var itemPrice = $(this).find('itemPrice').text();
-    var itemBrand = $(this).find('itemBrand').text();
-    var itemCategory = $(this).find('itemCategory').text();
-    var itemDescription = $(this).find('itemDescription').text();
-    var itemColor = $(this).find('itemColor').text();
-    var itemRating = $(this).find('itemRating').text();
-    var itemStock = $(this).find('itemStock').text();
-    var itemGender = $(this).find('itemGender').text();
-    var itemSize = $(this).find('itemSize').text();
+    var itemID = $(this).find('itemid').text();
+    var itemName = $(this).find('itemname').text();
+    var itemPrice = $(this).find('itemprice').text();
+    var itemBrand = $(this).find('itembrand').text();
+    var itemCategory = $(this).find('itemcategory').text();
+    var itemDescription = $(this).find('itemdescription').text();
+    var itemColor = $(this).find('itemcolor').text();
+    var itemRating = $(this).find('itemrating').text();
+    var itemStock = $(this).find('itemstock').text();
+    var itemGender = $(this).find('itemgender').text();
+    var itemSize = $(this).find('itemsize').text();
     $('<tr></tr>').html('<td>'+itemID+'</td><td>'+itemName+'</td><td>'+itemPrice+'</td><td>'+itemBrand+'</td><td>'+itemCategory+'</td><td>'+itemDescription+'</td><td>'+itemColor+'</td><td>'+itemRating+'</td><td>'+itemStock+'</td><td>'+itemGender+'</td><td>'+itemSize+'</td>').appendTo('#productxml');
    });
 }

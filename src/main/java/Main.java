093ec73e13bde9c124c760169a85ba42a0c91092
charlesygl <charlesygl@gmail.com>
2016-04-08 02:38:07 -0400

    try

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,9 @@ import routes.*;
 public class Main {
 
   public static void main(String[] args) {
+      
+
+
 	/*  
 	try{
 		Class.forName("org.postgresql.Driver");
@@ -24,6 +27,26 @@ public class Main {
 
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
+     
+      //Apply headers across all routers for CORS
+    CorsFilter.apply();
+
+    //Fix CORS when posting by adding options respone for Preflight check
+    options("/*", (request,response)->{
+
+        String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+        if (accessControlRequestHeaders != null) {
+            response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+        }
+
+        String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+        if(accessControlRequestMethod != null){
+            response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+        }
+
+        return "OK";
+    });//end options
+      
     Object r1 = new ItemTest();
   }
 

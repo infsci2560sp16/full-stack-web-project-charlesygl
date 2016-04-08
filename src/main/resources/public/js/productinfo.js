f9b7485d73b3b411d2a5f4bb2e6b4d6a2dae5e8e
charlesygl <charlesygl@gmail.com>
2016-04-08 02:16:47 -0400

    try

--- src/main/resources/public/js/productinfo.js
+++ src/main/resources/public/js/productinfo.js
@@ -12,7 +12,7 @@ function handleData(data) {
         $.each(data, function (i, item) {
             trHTML += '<tr><td>' + item.ITEMID + '</td><td>' + item.ITEMNAME + '</td><td>' + item.ITEMPRICE + '</td><td>' + item.ITEMBRAND + '</td><td>' + item.ITEMCATEGORY + '</td><td>' + item.ITEMDESCRIPTION + '</td><td>' + item.ITEMCOLOR + '</td><td>' + item.ITEMRATING + '</td><td>' + item.ITEMSTOCK + '</td><td>' + item.ITEMGENDER + '</td><td>' + item.ITEMSIZE + '</td></tr>';
         });
-        $('#inventory').append(trHTML);
+        $('#productjson').append(trHTML);
 }
 
 
